24126b29b01e211379f9517cd5ab22f715b60902
Francojimenezcopati <fjcopati@gmail.com>
2024-01-10 00:20:13 -0300

    Add styles with bootstrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,3 +6,14 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description", "important"]
+        widgets= {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control form-control-sm'
+            }),
+            'important': forms.CheckboxInput(attrs={
+                'class': 'form-check-input'
+            })
+        }
